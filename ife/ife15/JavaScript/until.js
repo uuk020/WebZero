cc8ac16d5d3a7f4ae079133f5ee0a850d504afe4
uuk020 <jie893609357@163.com>
2016-10-03 00:07:38 +0800

    删除测试页面增加until.js

--- ife/ife15/JavaScript/until.js
+++ ife/ife15/JavaScript/until.js
@@ -1,51 +1,30 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-	<meta charset="UTF-8">
-	<title>DOM操作</title>
-</head>
-<body>
-	<div class="a1 a56 a3" id="a2" style="position:absolute;left:1000px;top:2000px;">11</div>
-  <div id="adom">
-    <div class="classa">adom>class</div>
-  </div>
-  <a href="#">a标签</a>
-  <div >data</div>
-<script>
-
-// 为element增加一个样式名为newClassName的新样式
-var a2 = document.getElementById("a2");
 //检测是否有该类名
 function hasClassName(element, hClass){
-	return element.className.match(new RegExp("(\\s|^)" + hClass + "(\\s|$)"));
+  return element.className.match(new RegExp("(\\s|^)" + hClass + "(\\s|$)"));
 }
-//hasClassName(a2,"a1");
 function addClass(element, newClassName) {
   // your implement
   if(hasClassName(element,newClassName)){
-  	console.log('已有此类名');
+    console.log('已有此类名');
   }else{
-  	element.className += " " + newClassName;
+    element.className += " " + newClassName;
   }
 }
-addClass(a2,'a4');
-// 移除element中的样式oldClassName
 function removeClass(element, oldClassName) {
-    // your implement
+  // your implement
   var classNames = element.className.split(/\s+/), // 获取类名集合剪切成数组
-  		pos, // 此变量是获取有类名的下标
-  		i,
-  		len;
+      pos, // 此变量是获取有类名的下标
+      i,
+      len;
   for(i = 0,len = classNames.length;i < len;i++){ //循环元素类名
-  	if(classNames[i] == oldClassName){ //如果数组中的一个className等于oldClassName ,把该元素的下标赋值pos 结束循环
-  		pos = i; 
-  		break;
-  	}
-  }		
+    if(classNames[i] == oldClassName){ //如果数组中的一个className等于oldClassName ,把该元素的下标赋值pos 结束循环
+      pos = i; 
+      break;
+    }
+  }   
   classNames.splice(pos, 1); //剪切已有元素的下标
-  element.className = classNames.join(" ");	//重新设置类名	 
+  element.className = classNames.join(" "); //重新设置类名   
 }
-//removeClass(a2,"a56");
 // 判断siblingNode和element是否为同一个父元素下的同一级的元素，返回bool值
 function isSiblingNode(element, siblingNode) {
     // your implement
@@ -61,7 +40,6 @@ function getPosition(element) {
     return obj;
     //当计算边界矩形时，会考虑视口区域（或其他可滚动元素）内的滚动操作，也就是说，当滚动位置发生了改变，top和left属性值就会随之立即发生变化（因此，它们的值是相对于视口的，而不是绝对的）。如果不希望属性值随视口变化，那么只要给top、left属性值加上当前的滚动位置（通过window.scrollX和window.scrollY），这样就可以获取与当前的滚动位置无关的常量值。
 }
-//getPosition(a2)
 // 实现一个简单的Query
 function querySelector(selector, root){
   var elements = [],
@@ -123,23 +101,102 @@ function $(selector) {
     return querySelector(selector,document)[0];
   }
 }
+// 给一个element绑定一个针对event事件的响应，响应函数为listener
+  /**
+   * 事件添加函数
+   * @param {object}   element  需要绑定事件的对象
+   * @param {string}   event    事件类型
+   * @param {function} listener 事件触发执行的函数
+   */
+  function addEvent(element, event, listener) {
+    // your implement
+    if (element.addEventListener) {
+      element.addEventListener(event, listener);
+    } else if(element.attachEvent) {
+      element.attachEvent("on" + event, listener); //低版本IE浏览器
+    }else{
+      element["on" + event] = listener;
+    }
+  }
 
-// 可以通过id获取DOM对象，通过#标示，例如
-$("#adom"); // 返回id为adom的DOM对象
+  // 例如：
+  function clicklistener(event) {
+    ...
+  }
+  addEvent($("#doma"), "click", a);
+  /**
+   * 事件添加函数
+   * @param {object}   element  需要移除事件的对象
+   * @param {string}   event    事件类型
+   * @param {function} listener 事件被移除的函数
+   */
+  // 移除element对象对于event事件发生时执行listener的响应
 
-// 可以通过tagName获取DOM对象，例如
-$("a"); // 返回第一个<a>对象
+  function removeEvent(element, event, listener) {
+    // your implement
+    if (element.removeEventListenr) {
+      element.removeEventListenr(event, listener);
+    }else if (element.detachEvent) {
+      element.detachEvent("on" + event, listener);
+    } else {
+      element["on" + event] = null;
+    }
+  }
+  // 实现对click事件的绑定
+function addClickEvent(element, listener) {
+    // your implement
+  addEvent(element,"click",listener);  
+}
 
-// 可以通过样式名称获取DOM对象，例如
-$(".classa"); // 返回第一个样式定义包含classa的对象
-
-// 可以通过attribute匹配获取DOM对象，例如
-$("[data-log]"); // 返回第一个包含属性data-log的对象
-
-$("[data-time=2015]"); // 返回第一个包含属性data-time且值为2015的对象
-
-// 可以通过简单的组合提高查询便利性，例如
-console.log($("#adom .classa")); // 返回id为adom的DOM所包含的所有子节点中，第一个样式定义包含classa的对象
-</script>	
-</body>
-</html>
+// 实现对于按Enter键时的事件绑定
+function addEnterEvent(element, listener) {
+    // your implement
+   addEvent(element,"keydown",function(e){
+    var event = e || window.event;
+    if(event.keyCode === 13){
+      listener();
+    }
+   }); 
+}
+  /**
+   *结合DOM.html运用  
+   *addEvent(element, event, listener) -> $.on(element, event, listener);
+   *removeEvent(element, event, listener) -> $.un(element, event, listener);
+   *addClickEvent(element, listener) -> $.click(element, listener);
+   *addEnterEvent(element, listener) -> $.enter(element, listener);
+   */
+   $.on = function(element, event, listener) {
+    var el = element,
+        tp = event,
+        le = listener;
+    return addEvent(el, tp, le); 
+   }
+   $.un = function(element, event, listener) {
+    var el = element,
+        tp = event,
+        le = listener;
+    return removeEvent(el, tp, le);     
+   }
+   $.click = function(element, listener) {
+    var el = element,
+        le = listener;
+    return addClickEvent(el, le);    
+   }
+   $.enter = function(element, listener) {
+    var el = element,
+        le = listener;
+    return addEnterEvent(el, le);    
+   }
+   $.delegate = delegateEvent;
+  // 先简单一些
+  function delegateEvent(element, tag, eventName, listener) {
+    // your implement
+    return addEvent(element, eventName, function(ev) {
+      var oEvent = ev || window.event,
+          target = oEvent.target || oEvent.srcElement;
+      if(target.nodeName.toLowercase() === tag){
+        listener.call(target, oEvent);
+      }   
+    });
+  }
+ 
