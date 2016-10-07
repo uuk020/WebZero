f6b9713cacdcc88364b91a56fc940f4562277314
uuk020 <jie893609357@163.com>
2016-10-08 00:34:06 +0800

    修改小练习一的bug

--- ife/ife15/JavaScript/task0002_1.js
+++ ife/ife15/JavaScript/task0002_1.js
@@ -1,27 +1,25 @@
 /**
  * Created by Administrator on 2016/10/7.
  */
-(function handle(){
+(function handle() {
     var userInput = $("#user_input");
     var userOutput = $("#user_output");
-    $.click("#btn", function(){
+    $.click("#btn", function () {
         var value = userInput.value.split(/\n|\,|\，|\、|\s+|\u3000|\;/),
-            unvalue = uniqArray(value),
-            len = unvalue.length,
+            unValue = uniqArray(value),
+            len =  unValue.length,
             i = 0;
-        if (unvalue == "" || len > 10) {
-            $("#warn").style.display = "block";
+        if (len > 10 || unValue == "" ) {
+            $("p").style.display = "block";
         } else {
-            for ( ;i < len;i++) {
-                $("#warn").style.display = "none";
-                var trimValue = trim(unvalue[i]);
-                console.log(trimValue);
+            $("p").style.display = "none";
+            userOutput.innerHTML = "";
+            for (; i < len; i++) {
+                var trimValue = trim(unValue[i]);
                 if (trimValue !== "") {
                     userOutput.innerHTML += "<label>" + "<input type='checkbox'>"+ trimValue +"</label>";
                 }
             }
         }
-
-
     });
 })();
