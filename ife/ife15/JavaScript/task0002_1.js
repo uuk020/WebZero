d58563ba40185d04372b48b5a1ed4bbe455cf64f
uuk020 <jie893609357@163.com>
2016-10-07 23:37:08 +0800

    完成task02小练习一

--- ife/ife15/JavaScript/task0002_1.js
+++ ife/ife15/JavaScript/task0002_1.js
@@ -5,15 +5,23 @@
     var userInput = $("#user_input");
     var userOutput = $("#user_output");
     $.click("#btn", function(){
-        var value = userInput.value.split(/\,|\，/);
-        var unvalue = uniqArray(value);
-        for (var i = 0,len = unvalue.length;i < len;i++) {
-            var trimValue = trim(unvalue[i]);
-            console.log(trimValue);
-            if (trimValue !== "") {
-                userOutput.innerHTML += "<div>" + trimValue + "</div>"
+        var value = userInput.value.split(/\n|\,|\，|\、|\s+|\u3000|\;/),
+            unvalue = uniqArray(value),
+            len = unvalue.length,
+            i = 0;
+        if (unvalue == "" || len > 10) {
+            $("#warn").style.display = "block";
+        } else {
+            for ( ;i < len;i++) {
+                $("#warn").style.display = "none";
+                var trimValue = trim(unvalue[i]);
+                console.log(trimValue);
+                if (trimValue !== "") {
+                    userOutput.innerHTML += "<label>" + "<input type='checkbox'>"+ trimValue +"</label>";
+                }
             }
         }
+
 
     });
 })();
