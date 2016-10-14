cf52748fcd1ddfa3fa10b7b15c5b7ce0363ccac3
uuk020 <jie893609357@163.com>
2016-10-14 23:52:54 +0800

    完成练习三

--- ife/ife15/JavaScript/task0002_3.js
+++ ife/ife15/JavaScript/task0002_3.js
@@ -4,13 +4,14 @@
 var inValue = $(".in").value, //获取正序的值
     outValue = $(".out").value,//获取逆序的值
     loop = $(".loop").value,//获取循环的值
-    time = $("input").value || 4000,//获取时间隔间的值，默认值为4000
+    time = 4000,//获取时间隔间的值，默认值为4000
     play = null;//自动播放定时器
 
 function slideShow () {
     var slide = $(".slide"),
         imgItems = slide.getElementsByTagName("img"), //获取图片数量
         imgLength = imgItems.length,//缓存图片数量
+        slideLi = slide.getElementsByTagName("li"),
         imgWidth = imgItems[0].width,
         createNav = document.createElement("ol");//创建小圆点
     createNav.className = "slide-nav";
@@ -33,19 +34,22 @@ function slideShow () {
         });
     }
 
-    $.click(".in", function(){
+    $.click(".in", function() {
         inValue = 'true';
         $(".in").value = 'true';
         outValue = '';
         $(".out").value = '';
     });
-    $.click(".out", function(){
+    $.click(".out", function() {
         inValue = '';
         $(".in").value = '';
         outValue = 'true';
         $(".out").value = 'true';
     });
-    $.click(".loop");
+    $.click(".loop", function() {
+        loop = 'true';
+        $(".loop").value = 'true';
+    });
     /*
      *  循环轮播，循环时触发（正向与反向循环）
      */
@@ -53,7 +57,7 @@ function slideShow () {
         var lightLi = $(".slide-nav .active"),
             iTargets;
         //循环
-        if (loop) {
+        if (loop === 'true') {
             //正向循环
             if (inValue === 'true') {
                 iTargets = (getIndex(lightLi) + 1) === imgLength ? 0 : (-imgWidth * (getIndex(lightLi) + 1));
@@ -81,11 +85,41 @@ function slideShow () {
                 }
                 moveMargin(slide, iTargets);
             }
+        } else {
+            //正向循环
+            if (inValue === 'true') {
+                if ((getIndex(lightLi) + 1) !== imgLength) {
+                    iTargets = (-imgWidth * (getIndex(lightLi) + 1));
+                    var nextLi = lightLi.nextElementSibling;
+                    //console.log(nextLi);
+                    if (nextLi) {
+                        removeLiClass();
+                        addClass(nextLi, "active");
+                    } else {
+                        removeLiClass();
+                        addClass($(".slide-nav li"), "active");
+                    }
+                }
+                moveMargin(slide, iTargets);
+            }
+            //反向循环;
+            if (outValue === 'true') {
+                if (getIndex(lightLi) !== 0) {
+                    iTargets = -imgWidth * (getIndex(lightLi) - 1);
+                    var prevLi = lightLi.previousElementSibling;
+                    if (prevLi) {
+                        removeLiClass();
+                        addClass(prevLi,"active");
+                    } else {
+                        removeLiClass();
+                        addClass(createNav.getElementsByTagName("li")[imgLength - 1], "active");
+                    }
+                    moveMargin(slide, iTargets);
+                }
+            }
         }
 
     }
-
-
     //移除所以li的样式active
     function removeLiClass() {
         var oLi = createNav.getElementsByTagName("li");
@@ -96,5 +130,3 @@ function slideShow () {
 
 }
 slideShow();
-
-
