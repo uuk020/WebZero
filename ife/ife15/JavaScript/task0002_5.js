2d856ce2996ef57febe13d60ce59097b73212459
uuk020 <jie893609357@163.com>
2016-10-30 16:12:43 +0800

    完成前端学院练习四

--- ife/ife15/JavaScript/task0002_5.js
+++ ife/ife15/JavaScript/task0002_5.js
@@ -4,13 +4,13 @@
 window.onload = function () {
     var iText = $("input"),
         iList = $("ul");
-    inputChange();
+
     /**
      * 输入监听事件
      */
     function inputChange () {
         if (iText.addEventListener) {
-            iText.addEventListener("input",onInput,false);
+            iText.addEventListener("input", onInput,false);
         } else if (iText.attachEvent) {
             iText.attachEvent("OnPropChanged",onPropChanged);
         }
@@ -30,19 +30,18 @@ window.onload = function () {
      *  处理输入值
     */
     function handleValue (value) {
-        var isString = "";
         var reg = new RegExp("^" + value, "i");
         if (value === "") {
             iList.style.display = "none";
         } else {
-            ajax("data.txt", {
+            ajax("data.txt",{
                 onsuccess: prompt
             });
         }
         /*
          * AJAX 成功后执行函数
          */
-        function prompt (data) {
+        function  prompt (data) {
             var promptArr = eval(data);
             var liElement = "";
             for (var i = 0,len = promptArr.length; i < len; i++) {
@@ -56,4 +55,88 @@ window.onload = function () {
         }
     }
 
+    /*
+     * 鼠标点击、移入和移开事件
+     */
+    function clickEvent () {
+        delegateEvent(iList, "li", "mouseover", function () {
+           removeliClass();
+           addClass(this, "active");
+        });
+        delegateEvent(iList, "li", "mouseout", function () {
+           removeClass(this, "active");
+        });
+        delegateEvent(iList, "li", "click", function() {
+           iText.value = deleteSpan(this.innerHTML);
+           iList.style.display = "none";
+        });
+    }
+
+    /*
+     * 键盘上下和回车事件
+     */
+    function keyEvent () {
+        addEvent(iText, "keydown", function (ev) {
+            var lightLi = $(".active");
+            var oEvent = ev || window.event;
+            //向上键
+            if (oEvent.keyCode === 38) {
+                if (lightLi) {
+                    var prevElement = lightLi.previousElementSibling;
+                    if (prevElement) {
+                        removeliClass();
+                        addClass(prevElement, "active");
+                    }
+                } else {
+                    addClass($("div li"), "active");
+                }
+            }
+            //向下键
+            if (oEvent.keyCode === 40) {
+                if (lightLi) {
+                    var nextElement = lightLi.nextElementSibling;
+                    if (nextElement) {
+                        removeliClass();
+                        addClass(nextElement, "active");
+                    }
+                } else {
+                    addClass($("div li"), "active");
+                }
+            }
+            //enter
+            if (oEvent.keyCode === 13) {
+                iText.value = deleteSpan(lightLi.innerHTML);
+                iList.style.display = "none";
+            }
+        });
+    }
+
+    /*
+     * 筛选span内容
+     */
+    function deleteSpan (string) {
+        var reg = /^<span>(\w+)<\/span>(\w+)/;
+        var stringArr = string.match(reg);
+        if (stringArr) {
+            return stringArr[1] + stringArr[2];
+        } else {
+            return "";
+        }
+    }
+
+    /*
+     * 删除所有的li样式
+     */
+    function removeliClass() {
+        var liList = iList.getElementsByTagName("li");
+        for (var i = 0, len = liList.length; i < len; i++) {
+            liList[i].className = "";
+        }
+    }
+
+    inputChange();
+
+    clickEvent();
+
+    keyEvent();
 };
